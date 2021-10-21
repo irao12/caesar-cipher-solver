1473d8258b2057c378ef05c592be532f57fc6480
Ivan Rao <ivanrao@Ivans-MacBook-Air.local>
2021-10-21 15:32:40 -0400

    added calculate_vector

--- funcs.cpp
+++ funcs.cpp
@@ -12,3 +12,22 @@ double find_distance(int v1[], int v2[], int length){
   }
   return sqrt(total);
 }
+
+void calculate_vector(double vector[], std::string encrypted_string){
+  int num_of_letters = 0;
+
+  for (int i = 0; i < encrypted_string.length(); i++){
+    if (islower(encrypted_string[i])){
+      num_of_letters++;
+      int ascii = (int) encrypted_string[i];
+      vector[ascii - 97]++;
+    }
+  }
+
+  for (int i = 0; i < 26; i++){
+    vector[i] = vector[i] / num_of_letters;
+  }
+  return;
+}
+
+//std::string solve(std::string encrypted_string)
--- funcs.h
+++ funcs.h
@@ -2,3 +2,5 @@
 
 // add prototypes here
 double find_distance(int v1[], int v2[], int length);
+
+void calculate_vector(double vector[], std::string encrypted_string);
