901a7070b444cfb5b389de2ec417fae852a56a11
Ivan Rao <ivanrao@Ivans-MacBook-Air.local>
2021-10-21 10:41:58 -0400

    added find_distance function

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <cmath>
 #include "funcs.h"
 
-// add functions here
 
+// add functions here
+double find_distance(int v1[], int v2[], int length){
+  double total = 0;
+  for (int i = 0; i < length; i++){
+    double difference = v1[i] - v2[i];
+    total += (difference * difference);
+  }
+  return sqrt(total);
+}
--- funcs.h
+++ funcs.h
@@ -1,3 +1,4 @@
 #pragma once
 
 // add prototypes here
+double find_distance(int v1[], int v2[], int length);
